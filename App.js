f677d91f8a3f6bdec53a32bd2e4ddbbd27c33383
Abhishek Kapoor <kapoorabhishek567@gmail.com>
2022-02-20 15:39:42 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,124 @@
+import React,{useState} from 'react'
+import {Text,View,Image,TouchableOpacity,StyleSheet} from 'react-native'
+//touchable opacity also has pressable as a option
+//components
+import DiceOne from './assests/dice1.png'
+import DiceTwo from './assests/dice2.png'
+import DiceThree from './assests/dice3.png'
+import DiceFour from './assests/dice4.png'
+import DiceFive from './assests/dice5.png'
+import DiceSix from './assests/dice6.png'
+
+const App =()=>{
+  // const uri = DiceFive;
+  const [uri,setUri] = useState(DiceOne)
+  const [uri2,setUri2] = useState(DiceSix)
+
+  const buttonPress=()=>{
+    let randomNumber = Math.floor(Math.random()*6) + 1
+    let randomNumber2 = Math.floor(Math.random()*6) + 1
+
+
+    switch(randomNumber){
+      case 1:
+        setUri(DiceOne)
+        break;
+    
+    case 2:
+        setUri(DiceTwo)
+        break;
+    
+    case 3:
+        setUri(DiceThree)
+        break;
+    
+    case 4:
+        setUri(DiceFour)
+        break;
+    
+    case 5:
+        setUri(DiceFive)
+        break;
+    
+    case 6:
+        setUri(DiceSix)
+        break;
+      
+    default:
+      setUri(DiceOne)
+      break
+    
+  }
+
+  switch(randomNumber2){
+    case 1:
+      setUri2(DiceOne)
+      break;
+  
+  case 2:
+      setUri2(DiceTwo)
+      break;
+  
+  case 3:
+      setUri2(DiceThree)
+      break;
+  
+  case 4:
+      setUri2(DiceFour)
+      break;
+  
+  case 5:
+      setUri2(DiceFive)
+      break;
+  
+  case 6:
+      setUri2(DiceSix)
+      break;
+    
+  default:
+    setUri2(DiceOne)
+    break
+  
+}
+
+};
+
+  return(
+    <>
+    <View style={styles.container}>
+    <Image style={styles.image} source={uri2}/>
+      <Image style={styles.image} source={uri}/>
+    <TouchableOpacity onPress={buttonPress}>        
+    <Text style={styles.button}>Re Roll Dice</Text>
+    </TouchableOpacity>
+    </View>
+    </>
+  )
+}
+
+
+export default App
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:"#222831",
+    alignItems:"center", 
+    justifyContent:"center" 
+  },
+  image:{
+    width: 200,
+    height:200
+  },
+  button:{
+    fontSize: 20,
+    marginTop: 30,
+    color: "#F2A365",
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+    borderColor: "#30475E",
+    borderRadius: 5,
+    borderWidth: 3,
+    fontWeight: "bold"
+  }
+})
---
+++ README.md
@@ -0,0 +1,38 @@
+<h1 align="center">Welcome to Dice Roller 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000" />
+</p>
+
+> A simple Dice Roller Application with 2 different dices getting rolled at the same time and thus providing with random values in the dice, the app is developed with the help of React Native
+
+## Install
+
+```sh
+npm install
+```
+
+## Usage
+
+```sh
+npx react-native start
+```
+
+## Run tests
+
+```sh
+npx react-native run-android
+```
+
+## Author
+
+👤 **Abhishek Kapoor**
+
+* Github: [@Abhishek9111](https://github.com/Abhishek9111)
+* LinkedIn: [@Abhishek9111](https://linkedin.com/in/Abhishek9111)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
